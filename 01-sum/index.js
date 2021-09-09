6b553dabb5a1c217a5ccb35bbe22f5771acbcf42
ivankvach <ivankvach6@gmail.com>
2021-09-09 21:24:02 +0300

    1 stage

---
+++ 01-sum/index.js
@@ -0,0 +1,11 @@
+/**
+ * sum - returns sum of two numbers
+ * @param {number} a first number
+ * @param {number} b second number
+ * @returns {number}
+ */
+ sum(1, 1); // 2
+ sum(2, 5); // 7
+export default function sum(a, b) {
+  return a + b;
+}
